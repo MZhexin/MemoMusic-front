b6e85d24baad8a6f13c1bb27659054114af45751
Nagi-ovo <13264500190@163.com>
2023-03-28 17:18:23 +0800

    updated:css styles & title

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,38 +1,50 @@
-import React, { Component } from "react";
-import { NavLink, Link } from "react-router-dom";
-import {Navbar, Nav} from 'react-bootstrap';
+import React, { Component } from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import "./css/style.css";
+import './css/style.css';
 
 class NavBar extends Component {
   render() {
     const { handleLogOut, loggedIn, name } = this.props;
     return (
-      <Navbar style={{background: "#141a20"}} variant="dark" expand="lg">
-        <Link to="/"><Navbar.Brand>音乐推荐实验</Navbar.Brand></Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <NavLink to="/faq" className="navbar-nav nav-link mr-auto" style={{"color": "#999999"}}>
+      <Navbar style={{ background: '#141a20' }} variant='dark' expand='lg'>
+        <Link to='/'>
+          <Navbar.Brand className='memo-title'>MemoMusic</Navbar.Brand>
+        </Link>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='nav-intro'>
+            <NavLink to='/faq' className='navbar nav-link'>
               项目介绍
-          </NavLink>
+            </NavLink>
+          </Nav>
+
           <Nav>
-            {!loggedIn &&
-                <NavLink to="/login" className="nav-link">
-                  登录
-                </NavLink>
-            }
-            {!loggedIn &&
-              <NavLink to="/register" className="nav-link">
+            {!loggedIn && (
+              <NavLink
+                to='/login'
+                className='nav-link '
+                style={{ marginRight: '20px' }}
+              >
+                登录
+              </NavLink>
+            )}
+            {!loggedIn && (
+              <NavLink to='/register' className='nav-link '>
                 注册
               </NavLink>
-            }
-            {loggedIn &&
-                <Nav.Link>{"你好，"+name}</Nav.Link>
-            }
-            {loggedIn &&
-                <Nav.Link onClick={() => handleLogOut()}>退出</Nav.Link>
-            }
+            )}
+            {loggedIn && <Nav.Link>{'你好，' + name}</Nav.Link>}
+            {loggedIn && (
+              <Nav.Link
+                onClick={() => handleLogOut()}
+                style={{ marginLeft: '20px' }}
+              >
+                退出
+              </Nav.Link>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
